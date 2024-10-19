24fca6964226c0ab227d5d2df04dc2e4bc4c26a8
MinhPro999 <huuminh.cv@gmail.com>
2024-10-19 21:29:48 +0700

    thay doi la ban theo tuoi

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -19,20 +19,28 @@ class HomeScreen extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
+                child: Column(
                   children: [
-                    compassE('La bàn Đông tứ mệnh'),
-                    compassW('La bàn Tây tứ mệnh'),
-                    compassNomal('La bàn cơ bản'),
-                    compass24('La bàn 24 sơn hướng'),
+                    // Khoảng cách giữa GridView và mục cuối
                     compassOld('La bàn bát trạch theo tuổi'),
+                    const SizedBox(height: 16), // Chiếm toàn bộ chiều ngang
+                    Expanded(
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        children: [
+                          compassE('La bàn Đông tứ mệnh'),
+                          compassW('La bàn Tây tứ mệnh'),
+                          compassNomal('La bàn cơ bản'),
+                          compass24('La bàn 24 sơn hướng'),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
--- lib/wedget/wedget_gidview.dart
+++ lib/wedget/wedget_gidview.dart
@@ -1,83 +1,48 @@
-// Widget cho từng tùy chọn
 import 'package:flutter/material.dart';
 
 Widget compassE(String title) {
-  return Card(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10),
-    ),
-    elevation: 4,
-    child: Center(
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    ),
-  );
+  return _buildCard(title);
 }
 
 Widget compassW(String title) {
-  return Card(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10),
-    ),
-    elevation: 4,
-    child: Center(
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    ),
-  );
+  return _buildCard(title);
 }
 
 Widget compassNomal(String title) {
-  return Card(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10),
-    ),
-    elevation: 4,
-    child: Center(
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    ),
-  );
+  return _buildCard(title);
 }
 
 Widget compass24(String title) {
-  return Card(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10),
-    ),
-    elevation: 4,
-    child: Center(
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
+  return _buildCard(title);
+}
+
+Widget compassOld(String title) {
+  return SizedBox(
+    width: double.infinity, // Chiếm toàn bộ chiều ngang màn hình
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
         ),
-        textAlign: TextAlign.center,
       ),
     ),
   );
 }
 
-Widget compassOld(String title) {
+// Hàm dùng chung để tạo các Card widget
+Widget _buildCard(String title) {
   return Card(
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(10),
